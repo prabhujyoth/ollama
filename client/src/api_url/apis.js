492493bb713bc64bd4e413b074f0998c9d1d0b1b
PJ <prabhujyoth@gmail.com>
2025-04-06 00:03:17 +0530

    commit april 6th

--- client/src/api_url/apis.js
+++ client/src/api_url/apis.js
@@ -1,4 +1,6 @@
 const API_KEY = "TLJKNSQKB0W14TXT";
-export const TIME_SERIES_URL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo`;
+export const TIME_SERIES_URL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&apikey=${API_KEY}&symbol=`;
 
 export const TICKER_URL = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&apikey=${API_KEY}`;
+
+export const TOP_GAINERS_LOSERS = `https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey=demo`;
--- client/src/pages/Stocks.jsx
+++ client/src/pages/Stocks.jsx
@@ -1,13 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { LineChart } from "@mui/x-charts/LineChart";
 import { TIME_SERIES_URL } from "../api_url/apis";
+import { TOP_GAINERS_LOSERS } from "../api_url/apis";
+import { Text, Paper, Title, Tabs, Table } from "@mantine/core";
 export default function Stocks() {
   const [chartsData, setChartsData] = useState([]);
+  const [topGainersData, setTopGainersData] = useState([]);
+  const [topLosersData, setTopLosersData] = useState([]);
+  const [topActiveData, setTopActiveData] = useState([]);
+  const [ticker, setTicker] = useState("");
+
+  useEffect(() => {
+    const fetchGainersData = async () => {
+      const response = await fetch(TOP_GAINERS_LOSERS);
+      const data = await response.json();
+      const topGainers = data["top_gainers"];
+      const topLosers = data["top_losers"];
+      const topActive = data["most_actively_traded"];
+      setTopGainersData(topGainers);
+      setTopLosersData(topLosers);
+      setTopActiveData(topActive);
+    };
+    fetchGainersData();
+  }, []);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(TIME_SERIES_URL);
+        const response = await fetch(`${TIME_SERIES_URL}${ticker}`);
         const data = await response.json(); // Await here!
         const timeSeries = data["Time Series (Daily)"];
 
@@ -25,25 +45,221 @@ export default function Stocks() {
         console.error("Error fetching data:", error);
       }
     };
+    if (ticker) {
+      fetchData();
+    }
+  }, [ticker]);
 
-    fetchData();
-  }, []); // Add dependency array to run only once
+  function handleTickerUpdate(ticker) {
+    if (ticker) {
+      setTicker(ticker);
+    }
+  }
+  const topGainersrows = topGainersData.map((element) => (
+    <Table.Tr
+      key={element.ticker}
+      onClick={() => handleTickerUpdate(element.ticker)}
+    >
+      <Table.Td>{element.ticker}</Table.Td>
+      <Table.Td>{element.price}</Table.Td>
+      <Table.Td>
+        <span
+          className={
+            Number(element.change_amount) < 0
+              ? "text-red-400"
+              : "text-green-400"
+          }
+        >
+          {element.change_amount}
+        </span>
+      </Table.Td>
+      <Table.Td>{element.change_percentage}</Table.Td>
+      <Table.Td>{element.volume}</Table.Td>
+    </Table.Tr>
+  ));
 
+  const topLosersRows = topLosersData.map((element) => (
+    <Table.Tr
+      key={element.ticker}
+      onClick={() => handleTickerUpdate(element.ticker)}
+    >
+      <Table.Td>{element.ticker}</Table.Td>
+      <Table.Td>{element.price}</Table.Td>
+      <Table.Td>
+        <span
+          className={
+            Number(element.change_amount) < 0
+              ? "text-red-400"
+              : "text-green-400"
+          }
+        >
+          {element.change_amount}
+        </span>
+      </Table.Td>
+      <Table.Td>{element.change_percentage}</Table.Td>
+      <Table.Td>{element.volume}</Table.Td>
+    </Table.Tr>
+  ));
+
+  const topActiveRows = topActiveData.map((element) => (
+    <Table.Tr
+      key={element.ticker}
+      onClick={() => handleTickerUpdate(element.ticker)}
+    >
+      <Table.Td>{element.ticker}</Table.Td>
+      <Table.Td>{element.price}</Table.Td>
+      <Table.Td>
+        <span
+          className={
+            Number(element.change_amount) < 0
+              ? "text-red-400"
+              : "text-green-400"
+          }
+        >
+          {element.change_amount}
+        </span>
+      </Table.Td>
+      <Table.Td>{element.change_percentage}</Table.Td>
+      <Table.Td>{element.volume}</Table.Td>
+    </Table.Tr>
+  ));
+
+  // Add dependency array to run only once
+  const [activeTab, setActiveTab] = useState("first");
   return (
-    <div>
-      <LineChart
-        xAxis={[
-          { data: chartsData.map((item) => item.time), scaleType: "point" },
-        ]}
-        series={[
-          {
-            data: chartsData.map((item) => item.price),
-            label: "Closing Price",
-          },
-        ]}
-        width={1600}
-        height={800}
-      />
+    <div className="p-4 h-full">
+      <div className="flex flex-row gap-4 h-3/4">
+        <Paper shadow="xs" p="xl" className="flex-1">
+          <Title order={2} className="pb-2">
+            <LineChart
+              xAxis={[
+                {
+                  data: chartsData.map((item) => item.time),
+                  scaleType: "point",
+                  sx: {
+                    ".MuiChartsAxis-tickLabel": { fill: "white" },
+                    ".MuiChartsAxis-label": { fill: "white" },
+                  },
+                },
+              ]}
+              yAxis={[
+                {
+                  sx: {
+                    // Y axis label and ticks
+                    ".MuiChartsAxis-tickLabel": { fill: "white" },
+                    ".MuiChartsAxis-label": { fill: "white" },
+                  },
+                },
+              ]}
+              series={[
+                {
+                  data: chartsData.map((item) => item.price),
+                  label: "Closing Price",
+                  showMark: false,
+                },
+              ]}
+              width={800}
+              height={550}
+            />
+          </Title>
+        </Paper>
+        <Paper shadow="xs" p="xl" className="flex-1">
+          <Tabs value={activeTab} onChange={setActiveTab}>
+            <Tabs.List>
+              <Tabs.Tab value="first">Top Gainers</Tabs.Tab>
+              <Tabs.Tab value="second">Top Losers</Tabs.Tab>
+              <Tabs.Tab value="third">Mostly Actively Traded</Tabs.Tab>
+            </Tabs.List>
+
+            <Tabs.Panel value="first">
+              <Table
+                className="w-full"
+                striped
+                highlightOnHover
+                withTableBorder
+                withColumnBorders
+              >
+                <Table.Thead className="w-full">
+                  <Table.Tr className="w-full">
+                    <Table.Th>Ticker</Table.Th>
+                    <Table.Th>Price</Table.Th>
+                    <Table.Th>Change Amount</Table.Th>
+                    <Table.Th>Change Percentage</Table.Th>
+                    <Table.Th>Volume</Table.Th>
+                  </Table.Tr>
+                </Table.Thead>
+                <Table.Tbody className="w-full">{topGainersrows}</Table.Tbody>
+              </Table>
+            </Tabs.Panel>
+            <Tabs.Panel value="second">
+              <Table
+                className="w-full"
+                striped
+                highlightOnHover
+                withTableBorder
+                withColumnBorders
+              >
+                <Table.Thead className="w-full">
+                  <Table.Tr className="w-full">
+                    <Table.Th>Ticker</Table.Th>
+                    <Table.Th>Price</Table.Th>
+                    <Table.Th>Change Amount</Table.Th>
+                    <Table.Th>Change Percentage</Table.Th>
+                    <Table.Th>Volume</Table.Th>
+                  </Table.Tr>
+                </Table.Thead>
+                <Table.Tbody className="w-full">{topLosersRows}</Table.Tbody>
+              </Table>
+            </Tabs.Panel>
+            <Tabs.Panel value="third">
+              <Table
+                className="w-full"
+                striped
+                highlightOnHover
+                withTableBorder
+                withColumnBorders
+              >
+                <Table.Thead className="w-full">
+                  <Table.Tr className="w-full">
+                    <Table.Th>Ticker</Table.Th>
+                    <Table.Th>Price</Table.Th>
+                    <Table.Th>Change Amount</Table.Th>
+                    <Table.Th>Change Percentage</Table.Th>
+                    <Table.Th>Volume</Table.Th>
+                  </Table.Tr>
+                </Table.Thead>
+                <Table.Tbody className="w-full">{topActiveRows}</Table.Tbody>
+              </Table>
+            </Tabs.Panel>
+          </Tabs>
+        </Paper>
+      </div>
+      <div className="flex flex-row gap-4 pt-4">
+        <Paper shadow="xs" p="xl" className="flex-1">
+          <Title order={2} className="pb-2">
+            High Cap
+          </Title>
+          <Paper shadow="md" radius="md" withBorder p="xl">
+            <Text>Company</Text>
+          </Paper>
+        </Paper>
+        <Paper shadow="xs" p="xl" className="flex-1">
+          <Title order={2} className="pb-2">
+            Mid Cap
+          </Title>
+          <Paper shadow="md" radius="md" withBorder p="xl">
+            <Text>Company</Text>
+          </Paper>
+        </Paper>
+        <Paper shadow="xs" p="xl" className="flex-1">
+          <Title order={2} className="pb-2">
+            Low Cap
+          </Title>
+          <Paper shadow="md" radius="md" withBorder p="xl">
+            <Text>Company</Text>
+          </Paper>
+        </Paper>
+      </div>
     </div>
   );
 }
